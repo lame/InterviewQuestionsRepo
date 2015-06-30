68eb4aa0ebf4db311f68ad73bd2f73f271bfa32f
Ryan Kuhl <rkk09c@gmail.com>
2015-06-30 13:33:44 -0400

    updated comments in resources, slightly better test

--- API/resources/vsm_resource.py
+++ API/resources/vsm_resource.py
@@ -8,6 +8,11 @@ from fnmatch import fnmatch
 class VSMResource(Resource):
 
     def __init__(self):
+        """
+        Setting up the cassandra-driver connection information
+        and prepared statements here. Prepared statements cut
+        query time in half when compared to CQLEngine.
+        """
         cluster = Cluster([cassandra_cluster_ip])
         self.session = cluster.connect(cassandra_default_keyspace)
         self.get_all_info = self.session.prepare('SELECT * FROM vehicle_by_serial_number\
@@ -22,6 +27,18 @@ class VSMResource(Resource):
 
         results = self.session.execute(self.get_all_info, (vsn_prefix, ))
 
+        """
+        The matching algorithm is simple and intuitive,
+        I made sure to avoid having to make any sorted lists.
+        The algorithm will pull all matching prefixes(first
+        4 of VSN) and run those againse SequenceMatcher which
+        will output a boolean value similar to regex, but fits
+        this usecase better. It will then decide whether to add
+        that match to the list or not depending on the levenshtein
+        distance match score. Although it looks like 'results'
+        will have the total result set in memory, it uses JIT row
+        retrieval
+        """
         for result in results:
             if fnmatch(vsn_remainder, result.remainder.replace('*', '?')):
                 ratio = SequenceMatcher(None, result.remainder, vsn_remainder).quick_ratio()
--- API/tests/test_vsn_results.py
+++ API/tests/test_vsn_results.py
@@ -7,6 +7,8 @@ os.sys.path.insert(0, parentdir)
 
 from app import app
 from models.connect_to_cluster import Conn
+from models.vehicle_by_serial_number import VehicleBySerialNumber
+from utils.data.csv_reader import CSVReader
 
 
 class TestVsnResults(TestCase):
@@ -17,10 +19,16 @@ class TestVsnResults(TestCase):
         return app
 
     def setUp(self):
-        pass
+        vsn = [['ABCD*V000000', '000001', '2015', 'Test1', 'test1', 'Premium Package'],
+               ['ABCD*V******', '000002', '2015', 'Test2', 'test2', 'Pleb Package'],
+               ['ABCD*V******', '000003', '2015', 'Test3', 'test3', 'Limited Sahara Durango']]
+        for row in vsn:
+            CSVReader.populate_vehicle_by_serial_number(None, row)
 
     def tearDown(self):
-        pass
+        VehicleBySerialNumber(prefix='ABCD', remainder='*V000000', vehicle_trim='000001').delete()
+        VehicleBySerialNumber(prefix='ABCD', remainder='*V******', vehicle_trim='000002').delete()
+        VehicleBySerialNumber(prefix='ABCD', remainder='*V******', vehicle_trim='000003').delete()
 
     def query_vsn(self, vsn):
         result = self.client.get('/api/vsn/{0}'.format(vsn))
@@ -32,18 +40,27 @@ class TestVsnResults(TestCase):
         self.assertEqual(len(result.json), 0)
 
     def test_results(self):
-        result = self.query_vsn('ZJTERV000000')
+        result = self.query_vsn('ABCDEV000000')
         self.assertEqual(result.status_code, 200)
         self.assertEqual(len(result.json), 1)
-        self.assertEqual(result.json[0]['serial_number'],  "ZJTE*V000000")
+        self.assertEqual(result.json[0]['serial_number'],  'ABCD*V000000')
+        self.assertEqual(result.json[0]['vehicle_trim'], 1)
+        self.assertEqual(result.json[0]['year'], 2015)
+        self.assertEqual(result.json[0]['make'], 'Test1')
+        self.assertEqual(result.json[0]['model'], 'test1')
+        self.assertEqual(result.json[0]['trim_name'], 'Premium Package')
 
     def test_not_include(self):
         lst = []
-        results = self.query_vsn('ZJTERV000001')
+        results = self.query_vsn('ABCDEV000001')
+
         for result in results.json:
             lst.append(result['serial_number'])
-        self.assertFalse("ZJTE*V000000" in lst)
-        self.assertEqual(len(results.json), 5)
+
+        self.assertFalse("ABCD*V000000" in lst)
+        self.assertEqual(len(results.json), 2)
+        self.assertEqual(results.json[0]['serial_number'],  'ABCD*V******')
+        self.assertEqual(results.json[1]['serial_number'],  'ABCD*V******')
 
 if __name__ == '__main__':
     unittest.main()
